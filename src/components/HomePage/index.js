9075f8a6c6df457f903e2b167245f1c6c62aa26b
Muhammad Zaki <zakimalik853@gmail.com>
2023-07-25 15:19:52 +0500

    lazy loading added in all components

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { GlobalStyle } from '../../globalStyles';
-import Hero from '../../components/Hero';
-import Products from '../../components/Products';
 import { productData, productDataTwo } from '../../components/Products/data';
-import Feature from '../../components/Feature';
-const index = () => {
+
+const Hero = lazy(() => import('../../components/Hero'));
+const Products = lazy(() => import('../../components/Products'));
+const Feature = lazy(() => import('../../components/Feature'));
+
+const CenteredFallback = () => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh', // Take the full height of the viewport
+    }}
+  >
+    Loading...
+  </div>
+);
+
+const Index = () => {
   return (
     <div>
       <GlobalStyle />
-      <Hero />
-      <Products heading='Choose your favorite' data={productData} />
-      <Products heading='Sweet Treats for You' data={productDataTwo} />
-      <Feature />
+
+      <Suspense fallback={<CenteredFallback />}>
+        <Hero />
+        <Products heading='Choose your favorite' data={productData} />
+        <Products heading='Sweet Treats for You' data={productDataTwo} />
+        <Feature />
+      </Suspense>
     </div>
   );
 };
 
-export default index;
+export default Index;
